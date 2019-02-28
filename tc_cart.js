a2ef1ee7b0f72d82db88cee10f5b96bf0e60e553
Melinda Chirila <mchirila980@west-mec.org>
2019-02-28 15:28:38 -0700

    fixed some of the js, debugging in the cart javascript

--- tc_cart.js
+++ tc_cart.js
@@ -18,21 +18,21 @@ var orderTotal = 0;
 //this has the text string below,the cartHTML is going t o be made
 var cartHTML = "<table> <tr> <th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th> </tr>";
 //the loop is going to continue for the length of the items array. It starts at zero
-for (var i = 0; i < item.length; i++) {
+for (var i = 0; i <= 3; i++) {
     // this cartHTML is going to update with the the item #
     cartHTML += "<tr> <td><img src='tc_" + item[i] + ".png' alt='" + item[i] + "' /></td>";
     //this is going to change the items price and the quantity and the desc. of the item.
-    cartHTML += "<td>" + itemDescription[i] + "</td> <td>$" + itemPrice[i] + "</td> <td> " + itemQty[i] + "</td>";
+    cartHTML += "<td>" + itemDescription[i] + "</td>" + "<td>" + "$" + itemPrice[i] + "</td>" + "<td> " + itemQty[i] + "</td>";
     //the final items cost is going to be multiplied ny the quantity
     var itemCost = itemPrice[i] * itemQty[i];
     //the cartHTML is going to update only based on the items cost thats above.
-    cartHTML += "<td>$" + itemCost + "</td></tr>"
+    cartHTML += "<td>" + "$" + itemCost + "</td></tr>"
     //this is the order cost
     orderTotal += itemCost;
 }
 
 //The cartHTML will update the bases on the total cost of the order of the item
-cartHTML += "<tr><td colspan='4'>Subtotal</td><td>$" + orderTotal + "</td></tr></table>";
+cartHTML += "<tr><td colspan='4'>Subtotal</td><td>" + "$" + orderTotal + "</td>" + "</tr></table>";
 
 //The document is going to change so that it includes all the changes.
 document.getElementById("cart").innerHTML = cartHTML;
