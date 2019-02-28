31c589c5280ba839b4c01ede0afbda74b0e9f240
Melinda Chirila <mchirila980@west-mec.org>
2019-02-28 15:16:44 -0700

    the order js is ready

--- tc_order.js
+++ tc_order.js
@@ -16,4 +16,18 @@
    The itemQty array contains the quantity ordered of each item
    
 */
+//these are the ID item numbers.
+var item = [10582, 23015, 41807, 10041];
 
+//this is the items description
+var itemDescription = [
+    "1975 Green Bay Packers Football (signed), Item 10582",
+    "Tom Landry 1955 Football Card (unsigned), Item 23015",
+    "1916 Army-Navy Game, Framed Photo (signed), Item 41807",
+    "Protective Card Sheets, Item 10041"
+];
+//array for the prices of the items.
+var itemprice = [149.93, 89.98, 334.93, 22.67];
+
+//this is the array for the itemQty, these are the quantities that the custoer ordered of each item : 1, 1, 1, 4.
+var itemQty = [1, 1, 1, 4];
